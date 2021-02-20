f75961854345c09623cccf4b7e39922b7ade1f80
AntonyCoder <lysenko.tosha@gmail.com>
2021-02-20 15:27:43 +0300

    feat:add solution typical-arrays-problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
+ let min;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (i === 0) {
+                min = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+};
 exports.max = function max (array) {
-  return 0;
-}
+   let max;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (i === 0) {
+                max = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    let sum = 0;
+    let result;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        result = sum / array.length;
+        return result;
+    }
+};
